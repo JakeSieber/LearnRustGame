641bfe4b4e43eb174182f95336c198999cb50685
Will S <wsieber03@gmail.com>
2026-03-17 15:37:36 -0500

    restructure dirs

--- HelloWorldRust/hello_world/target/debug/deps/hello_world-e6c1b1edda2aafbc.d
+++
@@ -1,5 +0,0 @@
-/home/jacob/Desktop/HelloWorldRust/hello_world/target/debug/deps/hello_world-e6c1b1edda2aafbc.d: src/main.rs
-
-/home/jacob/Desktop/HelloWorldRust/hello_world/target/debug/deps/hello_world-e6c1b1edda2aafbc: src/main.rs
-
-src/main.rs:
--- HelloWorldRust/hello_world/target/debug/hello_world.d
+++
@@ -1,1 +0,0 @@
-/home/jacob/Desktop/HelloWorldRust/hello_world/target/debug/hello_world: /home/jacob/Desktop/HelloWorldRust/hello_world/src/main.rs
